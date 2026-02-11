1a1ec90898a0c098e81b4205c2facab63ca63799
saladtong <102506135+saladtong@users.noreply.github.com>
2026-02-10 18:53:15 -0800

    Formatting stimuli

--- experiment.js
+++ experiment.js
@@ -39,9 +39,9 @@ const trials = {
     {
       type: jsPsychHtmlAudioResponse,
       stimulus: `
-    <p style="font-size:48px; color:black;">${jsPsych.timelineVariable(
-      "stimulus"
-    )}</p>
+    <p style="font-size:48px; color:black;">${jsPsych
+      .timelineVariable("stimulus")
+      .toString()}</p>
     <p>Speak the above word.</p>`,
       recording_duration: 3500,
       show_done_button: true,
