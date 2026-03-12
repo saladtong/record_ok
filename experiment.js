becb78475083485826d9afbd02604bc2e872ce1b
saladtong <102506135+saladtong@users.noreply.github.com>
2026-03-11 20:49:46 -0700

    record prolific id

--- experiment.js
+++ experiment.js
@@ -5,7 +5,17 @@ const jsPsych = initJsPsych({
 });
 let timeline = [];
 
-const subject_id = jsPsych.randomization.randomID(10);
+// const subject_id = jsPsych.randomization.randomID(10);
+// capture info from Prolific
+var subject_id = jsPsych.data.getURLVariable("PROLIFIC_PID");
+var study_id = jsPsych.data.getURLVariable("STUDY_ID");
+var session_id = jsPsych.data.getURLVariable("SESSION_ID");
+
+jsPsych.data.addProperties({
+  subject_id: subject_id,
+  study_id: study_id,
+  session_id: session_id,
+});
 
 const irb = {
   // Which plugin to use
